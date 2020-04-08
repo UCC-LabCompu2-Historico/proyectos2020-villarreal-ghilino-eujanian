767bbebfd03d410b8d2dfb047add8cd70671219f
Ramiro Ghilino <ghilinoramiro@gmail.com>
2020-04-08 19:17:44 -0300

    Primeros archivos del juego

---
+++ Assets/Checkground.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkground : MonoBehaviour {
+
+	private Player_Controller player;
+
+
+
+    // Start is called before the first frame update
+    void Start() {
+        
+        player = GetComponentInParent<Player_Controller>();
+        
+    }
+
+   
+    void OnCollisionStay2D(Collision2D col){
+    	if(col.gameObject.tag == "Ground"){
+    		player.grounded = true;
+    	}
+    	
+    }
+
+     void OnCollisionExit2D(Collision2D col){
+     	if(col.gameObject.tag == "Ground"){
+    		player.grounded = false;
+    	}
+    	
+    }
+}
